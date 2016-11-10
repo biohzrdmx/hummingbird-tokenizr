e6743d17a7bd5f3e32327deaa7739cf3e01de4ce
biohzrdmx <biohazardmx@gmail.com>
2016-11-10 13:14:41 -0600

    Updated to 1.1

--- tokenizr.inc.php
+++ tokenizr.inc.php
@@ -3,7 +3,7 @@
 	/**
 	 * Tokenizr
 	 * @author 	biohzrdmx <github.com/biohzrdmx>
-	 * @version 1.0
+	 * @version 1.1
 	 * @license MIT
 	 * @example Basic usage:
 	 *
@@ -19,9 +19,11 @@
 	 *
 	 *      $data = Tokenizr::getData('something.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
 	 *
-	 *    Note that the data and its message digest are separated by a DOT, so it wouldn't be a good idea to try
-	 *    to tokenize strings containing dots (duh). Data structures (arrays/objects) aren't supported as-is, if
+	 *    Note that the data and its message digest are separated by a PERIOD, so it wouldn't be a good idea to try
+	 *    to tokenize strings containing periods (duh). Data structures (arrays/objects) aren't supported as-is, if
 	 *    you want to tokenize them, you'll have to serialize and/or base64-encode them before using this class.
+	 *
+	 *    NOTE: Since the last update you can change the divider to avoid collisions with PERIODS.
 	 *
 	 *    Also note that the data is saved in plain sight, THIS CLASS IS NOT DESIGNED TO ENCRYPT DATA. The purpose
 	 *    of this class is to provide a way to check if the data (from a cookie, for example) has been generated
@@ -30,20 +32,20 @@
 	 */
 	class Tokenizr {
 
-		static function getToken($data) {
+		static function getToken($data, $divider = '.') {
 			global $site;
 			$ret = false;
 			$key = $site->getGlobal('token_salt');
 			$hash = hash_hmac('sha256', $data, $key);
-			$ret = "{$data}.{$hash}";
+			$ret = "{$data}{$divider}{$hash}";
 			return $ret;
 		}
 
-		static function checkToken($token) {
+		static function checkToken($token, $divider = '.') {
 			global $site;
 			$ret = false;
 			$key = $site->getGlobal('token_salt');
-			$parts = explode('.', $token);
+			$parts = explode($divider, $token);
 			$data = get_item($parts, 0);
 			$hash = get_item($parts, 1);
 			if ($data && $hash) {
@@ -53,11 +55,11 @@
 			return $ret;
 		}
 
-		static function getData($token) {
+		static function getData($token, $divider = '.') {
 			global $site;
 			$ret = false;
 			$key = $site->getGlobal('token_salt');
-			$parts = explode('.', $token);
+			$parts = explode($divider, $token);
 			$ret = get_item($parts, 0);
 			return $ret;
 		}
